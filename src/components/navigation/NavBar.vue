423adf8b10b34ac534d31f8a2d72f35490d9cc1f
rahman <rahmannurhayadi@gmail.com>
2025-07-25 18:55:15 +0700

    feat(component) : add naviagtion

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="app-header bg-light border-0 navbar">
+        <button class="navbar-toggler sidebar-toggler d-lg-none mr-auto ml-3" type="button"
+            @click="$emit('toggle-sidebar')">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <a class="navbar-brand" href="#"><b>Back</b><span>strap</span></a>
+
+        <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" @click="$emit('toggle-sidebar')">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <ul class="nav navbar-nav d-md-down-none">
+            <li class="nav-item px-3"><a class="nav-link" href="#">Dashboard</a></li>
+            <li class="nav-item px-3"><a class="nav-link" href="#">Users</a></li>
+            <li class="nav-item px-3"><a class="nav-link" href="#">Settings</a></li>
+        </ul>
+
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="icon-bell"></i><span
+                        class="badge badge-pill badge-danger">5</span></a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="#"><img class="img-avatar" :src="img6" alt="admin" /></a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script setup>
+import img6 from '@/assets/img/avatars/6.jpg'
+</script>
---
+++ src/components/navigation/SideBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="sidebar sidebar-pills bg-transparent">
+        <nav class="sidebar-nav">
+            <ul class="nav">
+                <LinkNav name="Dashboard" icon="lni-dashboard" @click="selectMenu"/>
+                <li class="nav-title">Absence</li>
+                <LinkNav name="Create" icon="lni-target" @click="selectMenu"/>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+import LinkNav from '../utils/LinkNav.vue';
+const emits = defineEmits(['pickMenu'])
+const selectMenu = (name) => {
+  emits('pickMenu', name)
+}
+</script>a
