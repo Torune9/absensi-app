271bb3d55b966c7222e63626692d3374cd017e7b
rahman <rahmannurhayadi@gmail.com>
2025-07-25 18:54:57 +0700

    feat(component) : add alet

---
+++ src/components/utils/Alert.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="alert" role="alert" :class="`alert-${mode}`">
+        {{ message }}
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    mode: String,
+    message : String
+})
+</script>
