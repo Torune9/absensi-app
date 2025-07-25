d534a2ace529b29a5843c9d9125bbfd2f364174c
rahman <rahmannurhayadi@gmail.com>
2025-07-25 18:55:23 +0700

    feat(component) : add link naviagtion

---
+++ src/components/utils/LinkNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="nav-item">
+    <a class="nav-link" href="#" @click.prevent="handleClick">
+      <i :class="`lni ${icon}`"></i> {{ name }}
+    </a>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: String,
+  icon: String
+})
+const emit = defineEmits(['click'])
+
+const handleClick = () => {
+  emit('click', props.name)
+}
+</script>
